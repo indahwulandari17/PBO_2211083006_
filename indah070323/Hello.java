f033bbbb1d24378468e60e03b9bec1fb00352218
indahwulandari17 <indahwulandari130@gmail.com>
2023-03-16 10:44:44 +0700

    indah070323

---
+++ indah070323/Hello.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package indah070323;
+
+/**
+ *
+ * @author A409FA
+ */
+public class Hello {
+    public static void main (String[] args){
+        System.out.println("Hello World");
+    }
+} 
